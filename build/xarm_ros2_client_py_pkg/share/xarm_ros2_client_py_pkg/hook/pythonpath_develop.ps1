4d67f4d30e25ae373c6f44f2d47f21bac159f5e5
Scott Nortman <scott.nortman@gmail.com>
2023-05-07 21:50:19 -0400

    Checking in missing package files

---
+++ build/xarm_ros2_client_py_pkg/share/xarm_ros2_client_py_pkg/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/snortman/ros2_ws/src/xarm_ros2_client_py_pkg/build/xarm_ros2_client_py_pkg"
---
+++ build/xarm_ros2_client_py_pkg/share/xarm_ros2_client_py_pkg/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/snortman/ros2_ws/src/xarm_ros2_client_py_pkg/build/xarm_ros2_client_py_pkg"
